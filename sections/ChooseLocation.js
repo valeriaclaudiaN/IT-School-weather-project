88194e9a4c08ee84390314d23c2f2e5b9e652301
valeriaclaudiaN <neamtu.claudia2@yahoo.com>
2022-11-17 19:11:37 +0200

    removed comments

--- sections/ChooseLocation.js
+++ sections/ChooseLocation.js
@@ -4,9 +4,7 @@ const oradea = document.querySelector(".oradea");
 const currentCity = document.getElementById("current-city");
 
 const updateCity = (city) => {
-  // update the info
   currentCity.innerHTML = city;
-  // update the data from the API (current weather section)
   displayCurrentWeather(city);
 };
 
